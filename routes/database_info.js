bb9a8cb904bf4ac8266cd9565808b5d99d869be1
deng <978548481@qq.com>
2016-03-12 16:02:53 +0800

    加入内涵段子和不得姐

---
+++ routes/database_info.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var querystring = require('querystring');
+var util = require('util');
+var bodyParser = require('body-parser');
+var router = express.Router();
+var mysql = require('mysql');
+var http = require('http');
+var fs = require('fs');
+var conn = mysql.createConnection({
+  host:'localhost',
+  user:'root',
+  password:'dengyi',
+  database:'information_schema',
+  port:3306
+});
+conn.connect();
+/* post users listing. */
+router.use(bodyParser.urlencoded({
+  extended: true
+}));
+router.get('/', function(req, res, next) {
+  var  selectTableInfo = 'SELECT * FROM TABLES where TABLE_SCHEMA=\'srx\'';
+  conn.query(selectTableInfo,function(err, rows, fields){
+    if (err) {
+      res.json({msg:'err',content:'数据库错误'});
+
+    }else {
+      console.log(rows);
+      res.json({msg:'success',content:rows});
+    }
+
+
+
+
+  });
+
+});
+
+module.exports = router;
